84dd7beaca6ac232a28208841f7e7e8d5e316738
twoxey <id18_zzy@tongji.edu.cn>
2022-06-06 15:44:28 +0800

    changed element prototype and add slider group

--- util.js
+++ util.js
@@ -1,28 +1,47 @@
-function addUtilFunc(ele) {
-    ele.$attr = function (attr) {
-        Object.assign(this, attr)
-        return this
-    }
-    ele.$class = function (...className) {
-        this.className.add(...className)
-        return this
-    }
-    ele.$style = function (style) {
-        Object.assign(this.style, style)
-        return this
-    }
-    return ele
+Element.prototype.$attr = function (attr) {
+    Object.assign(this, attr)
+    return this
+}
+Element.prototype.$class = function (...className) {
+    this.className.add(...className)
+    return this
+}
+Element.prototype.$style = function (style) {
+    Object.assign(this.style, style)
+    return this
 }
 
 function ce(name, ...children) {
-    const ele = document.createElement(name)
-    ele.append(...children)
-    addUtilFunc(ele)
-    return ele
+    const el = document.createElement(name)
+    el.append(...children)
+    return el
+}
+function qs(query, parent = document) {
+    return parent.querySelector(query)
 }
 
-addEventListener('load', () => {
-    addUtilFunc(document.documentElement)
-    addUtilFunc(document.head)
-    addUtilFunc(document.body)
-})
+function slugify(str) {
+    return str.trim()
+        .replace(/[^\w\s-.~]/g, '')
+        .replace(/\s/g, '_')
+}
+
+class SliderGroup {
+    constructor(container) {
+        this.container = container || ce('div')
+    }
+    sliders = {}
+    attr = {}
+    value(name) { return this.sliders[name].value }
+    add(name, attr) {
+        const slider = ce('input').$attr({ type: 'range' })
+            .$attr(this.attr)
+            .$attr(attr)
+
+        const label = ce('label', name)
+        const value = ce('span', slider.value)
+        slider.addEventListener('input', () => value.textContent = slider.value)
+        this.sliders[name] = slider
+        this.container.append(label, slider, value, ce('br'))
+    }
+}
