2c34df665b77bda4dfbd33655dc9049b5437a988
twoxey <id18_zzy@tongji.edu.cn>
2023-03-03 16:41:27 +0800

    update changes

--- assets/utils.js
+++ assets/utils.js
@@ -14,12 +14,14 @@ const load_script = src => new Promise((resolve, reject) =>
         onerror: reject
     })()))
 
+const html_to_fragment = text => E('template', { innerHTML: text }).content
+
 const markdown = async text => {
     if (!window.marked) await load_script('https://cdnjs.cloudflare.com/ajax/libs/marked/4.2.12/marked.min.js')
-    return E('template', { innerHTML: marked.parse(text) })().content
+    return html_to_fragment(marked.parse(text))
 }
 
 const markdown_sync = text => {
     if (!window.marked) throw new Error('marked.js not loaded')
-    return E('template', { innerHTML: marked.parse(text) })().content
+    return html_to_fragment(marked.parse(text))
 }
